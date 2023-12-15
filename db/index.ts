fdfc4bd8df2c31efee40fc426cabc47de52f24b0
iMalFect <me@imal.dev>
2023-12-15 23:17:02 +0100

    ...

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-const queryClient = postgres({
-	host: process.env.POSTGRES_HOST!,
-	user: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD,
-	database: process.env.POSTGRES_DATABASE!
-});
+// @ts-ignore
+const queryClient = postgres(process.env.POSTGRES_URL);
 export default drizzle(queryClient, { schema });
