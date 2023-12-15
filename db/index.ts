4c34bd051177bf59c4a5ee543d384005b4584e90
iMalFect <me@imal.dev>
2023-12-15 23:01:46 +0100

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-/*const migrationClient = postgres({
-	host: process.env.DATABASE_HOST!,
-	port: Number(process.env.DATABASE_PORT),
-	user: process.env.DATABASE_USER,
-	password: process.env.DATABASE_PASSWORD,
-	database: process.env.DATABASE_NAME!,
-	max: 1
-});
-migrate(drizzle(migrationClient), './db/migrations');*/
 const queryClient = postgres({
 	host: process.env.POSTGRES_HOST!,
 	user: process.env.POSTGRES_USER,
