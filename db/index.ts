2cdcf5a7df62bc548396b6a15b2e7678dbb12c55
iMalFect <me@imal.dev>
2023-12-15 22:51:51 +0100

    Dead import

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import setUserHolding from './functions/setUserHolding';
 /*const migrationClient = postgres({
 	host: process.env.DATABASE_HOST!,
 	port: Number(process.env.DATABASE_PORT),
