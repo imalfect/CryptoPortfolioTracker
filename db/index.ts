14b226ef209597127c45273054cf25a5fa7ff3b9
iMalFect <me@imal.dev>
2023-12-15 23:21:38 +0100

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 // @ts-ignore
-const queryClient = postgres(process.env.POSTGRES_URL);
+const queryClient = postgres(process.env.POSTGRES_URL + '?ssl=true');
 export default drizzle(queryClient, { schema });
