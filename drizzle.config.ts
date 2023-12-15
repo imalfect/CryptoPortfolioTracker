d4aeb52742c0be749053489fdb0661ecfcf0604a
iMalFect <me@imal.dev>
2023-12-15 23:13:49 +0100

    Vercel cmon

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,15 @@
 import type { Config } from 'drizzle-kit';
 import 'dotenv/config';
 
-if (!process.env.DATABASE_URL) {
-	throw new Error('DATABASE_URL is missing');
+if (!process.env.POSTGRES_URL) {
+	throw new Error('POSTGRES_URL is missing');
 }
-
+console.log(process.env);
 export default {
 	schema: './db/schema.ts',
 	out: './db/migrations',
 	driver: 'pg',
 	dbCredentials: {
-		connectionString: process.env.POSTGRES_URL!
+		connectionString: process.env.POSTGRES_URL + '?ssl=true'
 	}
 } satisfies Config;
